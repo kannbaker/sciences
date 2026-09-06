0ff8e9b6946f89c898568e708333181f9c998072
kannbaker <thekanebacker@gmail.com>
2026-09-06 23:07:39 +0300

    gh pages 404 workaround

--- rspack.config.js
+++ rspack.config.js
@@ -11,7 +11,12 @@ const baseUrl = process.env.BASE_URL ?? env.BASE_URL ?? "";
 
 module.exports = {
   entry: "./src/index.ts",
-  output: { clean: true, filename: "assets/app.[contenthash].js", path: path.resolve(__dirname, "dist") },
+  output: {
+    clean: true,
+    filename: "assets/app.[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: baseUrl || "/"
+  },
   resolve: { extensions: [".ts", ".js"] },
   module: {
     rules: [
@@ -22,6 +27,7 @@ module.exports = {
   plugins: [
     new rspack.DefinePlugin({ __BASE_URL__: JSON.stringify(baseUrl) }),
     new rspack.HtmlRspackPlugin({ template: "./src/index.html" }),
+    new rspack.HtmlRspackPlugin({ filename: "404.html", template: "./src/index.html" }),
     new rspack.CopyRspackPlugin({ patterns: [{ from: "./src/favicon.svg", to: "favicon.svg" }] })
   ],
   devServer: { port: 3100, historyApiFallback: true, static: { directory: path.join(__dirname, "dist") } }
